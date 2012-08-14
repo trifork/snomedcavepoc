d205cf9cd566ff25d4f521e8da8557eeb48df8b2
Martin Westergaard Lassen <mwl@trifork.com>
2012-08-14 10:16:28 +0200

    Concept model

--- sc-web/src/main/java/dk/sst/snomedcave/model/Concept.java
+++ sc-web/src/main/java/dk/sst/snomedcave/model/Concept.java
@@ -1,5 +1,10 @@
 package dk.sst.snomedcave.model;
 
+import org.springframework.data.neo4j.annotation.Indexed;
+import org.springframework.data.neo4j.annotation.NodeEntity;
+
+@NodeEntity
 public class Concept {
+    @Indexed
     private String name;
 }
