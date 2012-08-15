86ddf4f410c5fac09dd878d8513948f6211071ad
Martin Westergaard Lassen <mwl@trifork.com>
2012-08-15 11:32:33 +0200

    Tree of concepts

--- sc-web/src/main/webapp/js/application.js
+++ sc-web/src/main/webapp/js/application.js
@@ -1,5 +1,29 @@
-function ConceptCtrl($scope) {
-    $scope.findConcept = function() {
-        $scope.concept = {name: "TEST: " + $scope.conceptName, childs: [{name: "Concept a", childs: 3}, {name: "Concept b", childs: 0}]}
+// Declare app level module which depends on filters, and services
+var module = angular.module('myApp', []);
+
+//var treeHtml = "<span>{{concept.name}}</span> <ul class=\"unstyled\"> <li ng-repeat=\"child in concept.childs\"> <a href=\"#"><i class="icon-plus-sign"></i></a> <div concept="child"></div> </li> </ul>"
+var treeHtml = "<span>{{concept.name}}</span><ul <li ng-repeat=\"child in concept.childs\"><div concept=\"child\"></div></li></ul>"
+
+module.controller("ConceptCtrl", function($scope) {
+    $scope.findConcept = function () {
+        $scope.concept = {name:"TEST: " + $scope.conceptName, childs:[
+            {name:"Concept a", childs:[
+                {name:"Concept c"}
+
+            ]},
+            {name:"Concept b", childs:[]}
+        ]
+        }
     }
-}
+});
+
+module.directive("concept", function($compile) {
+    return {
+        scope: {
+            concept: "="
+        },
+        link: function(scope, elem, attrs) {
+            return elem.append($compile(treeHtml)(scope))
+        }
+    };
+});
