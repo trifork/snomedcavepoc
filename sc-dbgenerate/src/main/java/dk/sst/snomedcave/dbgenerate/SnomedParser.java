ac293a127240b94accadfab1624005624f1dbd0c
Martin Westergaard Lassen <mwl@trifork.com>
2012-08-17 18:45:47 +0200

    Ability to parse SNOMED files into neo4j

--- sc-dbgenerate/src/main/java/dk/sst/snomedcave/dbgenerate/SnomedParser.java
+++ sc-dbgenerate/src/main/java/dk/sst/snomedcave/dbgenerate/SnomedParser.java
@@ -5,6 +5,7 @@ import dk.sst.snomedcave.model.Concept;
 import org.apache.log4j.Logger;
 import org.beanio.BeanReader;
 import org.beanio.StreamFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
 
@@ -12,11 +13,14 @@ import static java.lang.System.currentTimeMillis;
 
 public class SnomedParser {
     private static Logger logger = Logger.getLogger(SnomedParser.class);
-    private long lastModifiedConcept = 0L;
 
     ConceptRepository conceptRepository;
 
-    public void importJob() {
+    public SnomedParser(ConceptRepository conceptRepository) {
+        this.conceptRepository = conceptRepository;
+    }
+
+    public void startImport() {
         parseConcepts();
     }
 
@@ -24,26 +28,23 @@ public class SnomedParser {
         long startTime = currentTimeMillis();
         File conceptsFile = new File("/Users/mwl/IdeaProjects/SnomedCave/data/0/sct_concepts_20120813T134009.txt");
 
-        if (conceptsFile.lastModified() > lastModifiedConcept) {
-            logger.info("SNOMED concepts filed was changed. Initiating import");
-            lastModifiedConcept = conceptsFile.lastModified();
+        logger.info("SNOMED concepts filed was changed. Initiating import");
 
-            StreamFactory factory = StreamFactory.newInstance();
-            factory.load("/Users/mwl/IdeaProjects/SnomedCave/snomedcavepoc/sc-web/src/main/resources/beanio-concepts.xml");
-            BeanReader in = factory.createReader("concepts", conceptsFile);
+        StreamFactory factory = StreamFactory.newInstance();
+        factory.load("/Users/mwl/IdeaProjects/SnomedCave/snomedcavepoc/sc-dbgenerate/src/main/resources/beanio-concepts.xml");
+        BeanReader in = factory.createReader("concepts", conceptsFile);
 
-            Object record;
-            int count = 0;
-            int addedCount = 0;
-            int updatedCount = 0;
-            while ((record = in.read()) != null) {
-                if ("header".equals(in.getRecordName())) {
-                    //Map<String, Object> header = (Map<String, Object>) record;
-                    logger.debug("Parsing header: ignoring");
-                }
-                else if ("concept".equals(in.getRecordName())) {
-                    count++;
-                    Concept newConcept = (Concept) record;
+        Object record;
+        int count = 0;
+        int addedCount = 0;
+        int updatedCount = 0;
+        while ((record = in.read()) != null) {
+            if ("header".equals(in.getRecordName())) {
+                //Map<String, Object> header = (Map<String, Object>) record;
+                logger.debug("Parsing header: ignoring");
+            } else if ("concept".equals(in.getRecordName())) {
+                count++;
+                Concept newConcept = (Concept) record;
                     /*
                     Concept existingConcept = conceptRepository.getByConceptId(newConcept.getConceptId());
                     if (existingConcept != null) {
@@ -56,18 +57,16 @@ public class SnomedParser {
                         addedCount++;
                     }
                     */
-                    if (count % 1000 == 0) {
-                        logger.debug(String.format("time=%d, count=%d, added=%d, updated=%d", currentTimeMillis()-startTime, count, addedCount, updatedCount));
-                    }
-                    conceptRepository.save(newConcept);
+                if (count % 1000 == 0) {
+                    logger.debug(String.format("time=%d, count=%d, added=%d, updated=%d", currentTimeMillis() - startTime, count, addedCount, updatedCount));
                 }
-                else {
-                    logger.warn("unable to parse \"" + record + "\"");
-                }
+                conceptRepository.save(newConcept);
+            } else {
+                logger.warn("unable to parse \"" + record + "\"");
             }
-            in.close();
-            logger.info("Parsed " + count + " records in" + (currentTimeMillis() - startTime) + "ms from " + conceptsFile.getAbsolutePath());
         }
+        in.close();
+        logger.info("Parsed " + count + " records in" + (currentTimeMillis() - startTime) + "ms from " + conceptsFile.getAbsolutePath());
 
     }
 }
