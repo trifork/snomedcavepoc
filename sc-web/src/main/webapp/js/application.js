a10cc67a7c20dd9d94d25a943162783fe63c19e3
Martin Westergaard Lassen <mwl@trifork.com>
2012-08-22 21:33:01 +0200

    More nice UI

--- sc-web/src/main/webapp/js/application.js
+++ sc-web/src/main/webapp/js/application.js
@@ -10,6 +10,16 @@ var treeHtml = "<span>{{concept.fullyspecifiedName}}</span>" +
     "</ul>"
 //var treeHtml = "<span>{{concept.name}}</span><ul <li ng-repeat=\"child in concept.childs\"><div concept=\"child\"></div></li></ul>"
 
+module.controller("IdentityCtrl", function($scope) {
+    $scope.findIdentity = function() {
+        $("#identityResult").slideUp("slow")
+        $("#identityResult").slideDown("slow")
+    }
+    $scope.selectRegistration = function(number) {
+        alert("YEAH: " + number)
+    }
+})
+
 module.controller("ConceptCtrl", function($scope, $http) {
     $scope.findConcept = function () {
         $http.get("/concepts/search?query=" + $scope.conceptName).success(function(data, status) {
@@ -18,7 +28,7 @@ module.controller("ConceptCtrl", function($scope, $http) {
     }
 });
 
-//TODO: consider implementing @anderhessellund's example https://groups.google.com/forum/?fromgroups#!topic/angular/I5Z5oglW6Xw%5B1-25%5D
+//TODO: consider implementing @andershessellund's example https://groups.google.com/forum/?fromgroups#!topic/angular/I5Z5oglW6Xw%5B1-25%5D
 module.directive("concept", function($compile) {
     return {
         scope: {
