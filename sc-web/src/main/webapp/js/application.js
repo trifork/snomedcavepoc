74e48e9fc55092beb82bfd6a0ff62c05b985cb5e
Martin Westergaard Lassen <mwl@trifork.com>
2012-08-22 22:18:41 +0200

    Optimized and refactorings

--- sc-web/src/main/webapp/js/application.js
+++ sc-web/src/main/webapp/js/application.js
@@ -12,21 +12,18 @@ var treeHtml = "<span>{{concept.fullyspecifiedName}}</span>" +
 
 module.controller("IdentityCtrl", function($scope) {
     $scope.findIdentity = function() {
-        $("#identityResult").slideUp("slow")
-        $("#identityResult").slideDown("slow")
+        $scope.identityResult = $scope.identityCpr
     }
     $scope.selectRegistration = function(number) {
-        alert("YEAH: " + number)
+        $scope.selectedRegistration = "Some"
     }
-})
 
-module.controller("ConceptCtrl", function($scope, $http) {
     $scope.findConcept = function () {
         $http.get("/concepts/search?query=" + $scope.conceptName).success(function(data, status) {
             $scope.concept = data
         })
     }
-});
+})
 
 //TODO: consider implementing @andershessellund's example https://groups.google.com/forum/?fromgroups#!topic/angular/I5Z5oglW6Xw%5B1-25%5D
 module.directive("concept", function($compile) {
