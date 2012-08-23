bcf648c137d46b344402782f2f3d9a3970d92925
Martin Westergaard Lassen <mwl@trifork.com>
2012-08-23 08:41:38 +0200

    Sweet animations

--- sc-web/src/main/webapp/js/application.js
+++ sc-web/src/main/webapp/js/application.js
@@ -10,12 +10,12 @@ var treeHtml = "<span>{{concept.fullyspecifiedName}}</span>" +
     "</ul>"
 //var treeHtml = "<span>{{concept.name}}</span><ul <li ng-repeat=\"child in concept.childs\"><div concept=\"child\"></div></li></ul>"
 
-module.controller("IdentityCtrl", function($scope) {
+module.controller("IdentityCtrl", function($scope, $log) {
     $scope.findIdentity = function() {
         $scope.identityResult = $scope.identityCpr
     }
     $scope.selectRegistration = function(number) {
-        $scope.selectedRegistration = "Some"
+        $scope.selectedRegistration = "Some" + number
     }
 
     $scope.findConcept = function () {
@@ -23,6 +23,25 @@ module.controller("IdentityCtrl", function($scope) {
             $scope.concept = data
         })
     }
+    $scope.$watch("identityResult", function(newValue, oldValue) {
+        $scope.selectedRegistration = undefined
+        if (newValue) {
+            $("#identityResult").slideUp()
+            $("#identityResult").slideDown()
+        }
+        else {
+            $("#identityResult").slideUp()
+        }
+    })
+    $scope.$watch("selectedRegistration", function(newValue, oldValue) {
+        if (newValue) {
+            $("#conceptBrowser").slideUp()
+            $("#conceptBrowser").slideDown()
+        }
+        else {
+            $("#conceptBrowser").slideUp()
+        }
+    })
 })
 
 //TODO: consider implementing @andershessellund's example https://groups.google.com/forum/?fromgroups#!topic/angular/I5Z5oglW6Xw%5B1-25%5D
@@ -36,3 +55,11 @@ module.directive("concept", function($compile) {
         }
     };
 });
+
+$(document).ready(function() {
+    $("#conceptSearch").typeahead({
+        source: function(query, process) {
+            process(["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"])
+        }
+    })
+})
